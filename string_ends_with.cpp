0156dd7cb6b2a01c675e4f08d50cb153df1125dd
virawhy <6334980@stud.nau.edu.ua>
2024-05-26 13:01:26 +0300

    edited string ends with

--- string_ends_with.cpp
+++ string_ends_with.cpp
@@ -2,12 +2,16 @@
 #include <string>
 
 bool solution(std::string const &str, std::string const &ending) {
-    
+    std::string r_ending = std::string(ending.rbegin(), ending.rend());
+    for(int i = 0; i < r_ending.length(); i++)
+        if(str[str.length() - 1 - i] != r_ending[i]) return false;
     return true;
 }
 
 int main()
 {
-
+    std::cout << solution("abcde", "cde") << std::endl;
+    std::cout << solution("abcde", "abc") << std::endl;
+    std::cout << solution("abc", "") << std::endl;
     return 0;
 }
